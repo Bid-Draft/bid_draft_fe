ed3c938bcf88f1b37bea8cd4f1ddfc7f67bf139f
Drew Proebstel <d.proebstel@gmail.com>
2023-04-26 20:35:29 -0700

    all sets avaliable in create page

--- src/routes/create.jsx
+++ src/routes/create.jsx
@@ -47,9 +47,55 @@ const SelectSet = () => {
     label="Age"
     onChange= {handleChange} 
   >
-    <MenuItem value={"RTR"}>RTR</MenuItem>
-    <MenuItem value={"INS"}>INS</MenuItem>
+    <MenuItem value={"MOM"}>MOM</MenuItem>
+    <MenuItem value={"ONE"}>ONE</MenuItem>
+    <MenuItem value={"BRO"}>BRO</MenuItem>
+    <MenuItem value={"DMU"}>DMU</MenuItem>
+    <MenuItem value={"SNC"}>SNC</MenuItem>
+    <MenuItem value={"NEO"}>NEO</MenuItem>
+    <MenuItem value={"VOW"}>VOW</MenuItem>
+    <MenuItem value={"MID"}>MID</MenuItem>
+    <MenuItem value={"AFR"}>AFR</MenuItem>
+    <MenuItem value={"STX"}>STX</MenuItem>
+    <MenuItem value={"KHM"}>KHM</MenuItem>
+    <MenuItem value={"ZNR"}>ZNR</MenuItem>
+    <MenuItem value={"IKO"}>IKO</MenuItem>
+    <MenuItem value={"M21"}>M21</MenuItem>
+    <MenuItem value={"TBD"}>TBD</MenuItem>
+    <MenuItem value={"ELD"}>ELD</MenuItem>
+    <MenuItem value={"M20"}>M20</MenuItem>
+    <MenuItem value={"WAR"}>WAR</MenuItem>
+    <MenuItem value={"RNA"}>RNA</MenuItem>
+    <MenuItem value={"GRN"}>GRN</MenuItem>
+    <MenuItem value={"M19"}>M19</MenuItem>
+    <MenuItem value={"DOM"}>DOM</MenuItem>
+    <MenuItem value={"RIX"}>RIX</MenuItem>
+    <MenuItem value={"XLN"}>XLN</MenuItem>
+    <MenuItem value={"HOU"}>HOU</MenuItem>
+    <MenuItem value={"AKH"}>AKH</MenuItem>
+    <MenuItem value={"AER"}>AER</MenuItem>
+    <MenuItem value={"KLD"}>KLD</MenuItem>
+    <MenuItem value={"EMN"}>EMN</MenuItem>
+    <MenuItem value={"SOI"}>SOI</MenuItem>
+    <MenuItem value={"OGW"}>OGW</MenuItem>
+    <MenuItem value={"BFZ"}>BFZ</MenuItem>
+    <MenuItem value={"ORI"}>ORI</MenuItem>
+    <MenuItem value={"DTK"}>DTK</MenuItem>
+    <MenuItem value={"FRF"}>FRF</MenuItem>
     <MenuItem value={"KTK"}>KTK</MenuItem>
+    <MenuItem value={"M15"}>M15</MenuItem>
+    <MenuItem value={"JOU"}>JOU</MenuItem>
+    <MenuItem value={"BNG"}>BNG</MenuItem>
+    <MenuItem value={"THS"}>THS</MenuItem>
+    <MenuItem value={"M14"}>M14</MenuItem>
+    <MenuItem value={"DGM"}>DGM</MenuItem>
+    <MenuItem value={"GTC"}>GTC</MenuItem>
+    <MenuItem value={"RTR"}>RTR</MenuItem>
+    <MenuItem value={"M13"}>M13</MenuItem>
+    <MenuItem value={"AVR"}>AVR</MenuItem>
+    <MenuItem value={"DKA"}>DKA</MenuItem>
+    <MenuItem value={"INS"}>INS</MenuItem>
+    <MenuItem value={"M12"}>M12</MenuItem>
   </Select>
   <Button 
         onClick={() => makeGame(set)}
